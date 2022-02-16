68b055dd162a4dca02433b95b1d89e983da9e12f
torgeirgrini <torgeirgrini@gmail.com>
2022-02-16 17:40:38 +0100

    timer module implemented

--- src/localElevator/timer/timer.go
+++ src/localElevator/timer/timer.go
@@ -1,2 +1,30 @@
 package timer
 
+import (
+	"time"
+)
+
+func getWallTime() float64 {
+	timeNow := time.Now()
+	return float64(timeNow.Second()) + float64(timeNow.Nanosecond())/float64(1000000000)
+}
+
+var timerEndTime float64
+var timerActive int
+
+func TimerStart(duration float64) {
+	timerEndTime = getWallTime() + duration
+	timerActive = 1
+}
+
+func TimerStop() {
+	timerActive = 0
+}
+
+func TimerTimedOut() int {
+	timeRanOut := 0
+	if getWallTime() > timerEndTime {
+		timeRanOut = 1
+	}
+	return (timerActive & timeRanOut)
+}
