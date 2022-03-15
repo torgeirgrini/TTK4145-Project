f7dfb7a8392115c03895a8d26fccf8d9bc7af52c
randistensvand <randi.stensvand@gmail.com>
2022-03-15 20:45:56 +0100

    lols

--- src/distribution/distribution.go
+++ src/distribution/distribution.go
@@ -5,18 +5,26 @@ import (
 	"Project/localElevator/elevio"
 )
 
-func Distribution(ch_newLocalOrder chan elevio.ButtonType) {
+func Distribution(id string,
+	ch_newLocalOrder <-chan elevio.ButtonType,
+	ch_NewElevatorStateMap chan<- map[string]elevator.Elevator,
+	ch_newLocalElevator <-chan elevator.Elevator) {
+
 	elevatorSystemMap := make(map[string]elevator.Elevator)
 	for {
 		select {
 
 		case newOrder := <-ch_newLocalOrder:
-			elevatorSystemMap = updateMapWithOrder(elevatorSystemMap, newOrder)
-			hallcalls := activeHallCalls(elevatorSystemMap, newOrder)
-			ch_unassignedOrder <- hallcalls
+			elevatorSystemMap = updateMapWithOrder(id, elevatorSystemMap, newOrder)
+			ch_unassignedOrder <- elevatorSystemMap     //Til assigner
+			ch_NewElevatorStateMap <- elevatorSystemMap //Til network
 
-			// case newFloor := <-ch_FloorArrival:
+		case updatedElevatorSystemMap := <-ch_updatedElevatorSystemMap:
+			//sebd den ut på nettet!!
+			//send vår elevstruct til localElevator fsm
 
+		case newLocalElevator := <-ch_newLocalElevator:
+			elevatorSystemMap = updateMapWithLocalElevator(id, elevatorSystemMap, newLocalElevator)
 			// case <-ch_doorTimer:
 
 			// case obstruction = <-ch_Obstruction:
@@ -26,10 +34,12 @@ func Distribution(ch_newLocalOrder chan elevio.ButtonType) {
 
 }
 
-func updateMapWithOrder(id string,
-						elevatorSystemMap map[string]elevator.Elevator,
-						newOrder elevio.ButtonType) {
+func updateMapWithOrder(id string, elevatorSystemMap map[string]elevator.Elevator, newOrder elevio.ButtonType) map[string]elevator.Elevator {
+	//oppdater elevatorSystemMap[id]
+	return elevatorSystemMap
+}
 
-
-
+func updateMapWithLocalElevator(id string, elevatorSystemMap map[string]elevator.Elevator, newLocalElevator elevator.Elevator) map[string]elevator.Elevator {
+	//oppdater elevatorSystemMap[id]
+	return elevatorSystemMap
 }
